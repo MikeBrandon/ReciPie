572082b2843f42e115f5e8cb9374a7f68b75457f
Mike Brandon <bkb.braock@gmail.com>
2021-03-07 21:18:13 +0300

    Added ogin Validation

--- av-project/frontend/app/src/main/java/com/scorpysmurf/recipie2/loginsignup/LoginTabFragment.java
+++ av-project/frontend/app/src/main/java/com/scorpysmurf/recipie2/loginsignup/LoginTabFragment.java
@@ -1,6 +1,7 @@
 package com.scorpysmurf.recipie2.loginsignup;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,17 +15,22 @@ import androidx.fragment.app.Fragment;
 
 import com.scorpysmurf.recipie2.R;
 
+import java.util.regex.Pattern;
+
 public class LoginTabFragment extends Fragment {
 
     EditText email, pass;
     TextView forgotpass;
     Button login;
-    float v=0;
+    float v = 0;
+
+    private final static Pattern PASSWORD_PATTERN =
+            Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=\\S+$).{6,}$");
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.login_tab_fragment,container,false);
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.login_tab_fragment, container, false);
 
         email = root.findViewById(R.id.email);
         pass = root.findViewById(R.id.password);
@@ -46,7 +52,50 @@ public class LoginTabFragment extends Fragment {
         forgotpass.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(500).start();
         login.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(800).start();
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                validEmail();
+                validPassword();
+
+            }
+        });
+
         return root;
+    }
+
+    private boolean validEmail() {
+        String emailInput = email.getText().toString().trim();
+
+        if (emailInput.isEmpty()) {
+            email.setError(getString(R.string.field_cant_be_empty));
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            email.setError(getString(R.string.please_enter_a_valid_email_address));
+            return false;
+        } else {
+            email.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validPassword() {
+        String passInput = pass.getText().toString().trim();
+
+        if (passInput.isEmpty()) {
+            pass.setError(getString(R.string.field_cant_be_empty));
+            return false;
+        } else if(pass.length()<6) {
+            pass.setError(getString(R.string.short_pass));
+            return false;
+        }else if(!PASSWORD_PATTERN.matcher(passInput).matches()) {
+            pass.setError(getString(R.string.weak_pass));
+            return false;
+        } else {
+            pass.setError(null);
+            return true;
+        }
     }
 
 }
--- av-project/frontend/app/src/main/java/com/scorpysmurf/recipie2/loginsignup/SignupTabFragment.java
+++ av-project/frontend/app/src/main/java/com/scorpysmurf/recipie2/loginsignup/SignupTabFragment.java
@@ -1,9 +1,11 @@
 package com.scorpysmurf.recipie2.loginsignup;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
@@ -12,13 +14,100 @@ import androidx.fragment.app.Fragment;
 
 import com.scorpysmurf.recipie2.R;
 
+import java.util.regex.Pattern;
+
 public class SignupTabFragment extends Fragment {
+
+    EditText emailET, phoneET, passET, confPassET;
+    Button signUp;
+    private final static Pattern PASSWORD_PATTERN =
+            Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=\\S+$).{6,}$");
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.signup_tab_fragment,container,false);
 
+        emailET = root.findViewById(R.id.email);
+        phoneET = root.findViewById(R.id.mobile_number);
+        passET = root.findViewById(R.id.password);
+        confPassET = root.findViewById(R.id.confirm_password);
+        signUp = root.findViewById(R.id.sign_up_button);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validEmail();
+                validPassword();
+                validConfPassword();
+                validPhone();
+            }
+        });
+
         return root;
     }
+
+    private boolean validEmail() {
+        String emailInput = emailET.getText().toString().trim();
+
+        if (emailInput.isEmpty()) {
+            emailET.setError(getString(R.string.field_cant_be_empty));
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            emailET.setError(getString(R.string.please_enter_a_valid_email_address));
+            return false;
+        }else {
+            emailET.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validPassword() {
+        String passInput = passET.getText().toString().trim();
+
+        if (passInput.isEmpty()) {
+            passET.setError(getString(R.string.field_cant_be_empty));
+            return false;
+        } else if(passET.length()<6) {
+            passET.setError(getString(R.string.short_pass));
+            return false;
+        } else if(!PASSWORD_PATTERN.matcher(passInput).matches()) {
+            passET.setError(getString(R.string.weak_pass));
+            return false;
+        } else {
+            passET.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validConfPassword() {
+        String passInput = confPassET.getText().toString().trim();
+
+        if (passInput.isEmpty()) {
+            confPassET.setError(getString(R.string.field_cant_be_empty));
+            return false;
+        } else if(passET.length()<6) {
+            passET.setError(getString(R.string.short_pass));
+            return false;
+        } else if(!PASSWORD_PATTERN.matcher(passInput).matches()) {
+            confPassET.setError(getString(R.string.weak_pass));
+            return false;
+        } else {
+            confPassET.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validPhone() {
+        String passInput = phoneET.getText().toString().trim();
+
+        if (passInput.isEmpty()) {
+            phoneET.setError(getString(R.string.field_cant_be_empty));
+            return false;
+        } else {
+            phoneET.setError(null);
+            return true;
+        }
+    }
+
 }
