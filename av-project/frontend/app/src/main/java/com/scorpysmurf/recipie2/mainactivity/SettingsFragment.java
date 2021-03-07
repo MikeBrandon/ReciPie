c8b17d3209bca01a290156ef8e1424245a0e5462
Mike Brandon <bkb.braock@gmail.com>
2021-03-08 02:36:53 +0300

    Designed Settings Fragment Added Translations for French and Spanish

--- av-project/frontend/app/src/main/java/com/scorpysmurf/recipie2/mainactivity/SettingsFragment.java
+++ av-project/frontend/app/src/main/java/com/scorpysmurf/recipie2/mainactivity/SettingsFragment.java
@@ -9,10 +9,19 @@ import android.transition.TransitionInflater;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import com.scorpysmurf.recipie2.R;
 
+import java.util.Locale;
+
 public class SettingsFragment extends Fragment {
+
+    Spinner spinnerctrl;
+    Button btn;
+    Locale myLocale;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
