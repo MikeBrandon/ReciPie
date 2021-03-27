b0160d626f55cb394e3e6d6ff828e326da932a93
Mike Brandon <bkb.braock@gmail.com>
2021-03-27 16:39:49 +0300

    Added ability to save online pages to Cloud and retrieve them later.

--- av-project/frontend/app/src/main/java/com/scorpysmurf/recipie2/OnlineSavedRecipesActivity.java
+++ av-project/frontend/app/src/main/java/com/scorpysmurf/recipie2/OnlineSavedRecipesActivity.java
@@ -1,5 +1,6 @@
 package com.scorpysmurf.recipie2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
@@ -9,6 +10,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -17,15 +19,31 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class OnlineSavedRecipesActivity extends AppCompatActivity {
 
     ListView listView;
     public static ArrayAdapter arrayAdapter;
-    FloatingActionButton fabHelp;
+    FloatingActionButton fabHelp, fabDownload, fabUpload;
+    DocumentReference documentReference;
+    FirebaseAuth fAuth;
+    FirebaseUser user;
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +53,14 @@ public class OnlineSavedRecipesActivity extends AppCompatActivity {
 
         listView = findViewById(R.id.online_saves_list_view);
         fabHelp = findViewById(R.id.fab_help);
+        fabDownload = findViewById(R.id.fab_download);
+        fabUpload = findViewById(R.id.fab_upload);
+
+        fAuth = FirebaseAuth.getInstance();
+        user = fAuth.getCurrentUser();
+        fStore = FirebaseFirestore.getInstance();
+
+        documentReference = fStore.collection("users").document(user.getUid());
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -128,5 +154,73 @@ public class OnlineSavedRecipesActivity extends AppCompatActivity {
                 Toast.makeText(OnlineSavedRecipesActivity.this, OnlineSavedRecipesActivity.this.getString(R.string.long_to_delete), Toast.LENGTH_SHORT).show();
             }
         });
+
+        fabUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String uName = user.getUid();
+                String email = user.getEmail();
+
+                Map<String,Object> userUploadObject = new HashMap<>();
+                userUploadObject.put("uName",uName);
+                userUploadObject.put("email",email);
+
+                String[] titleArray = MainActivity.urlTitles.toArray(new String[0]);
+                String[] urlArray = MainActivity.urls.toArray(new String[0]);
+
+                userUploadObject.put("urlTitles", titleArray);
+                userUploadObject.put("urls", urlArray);
+
+                documentReference.set(userUploadObject).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(OnlineSavedRecipesActivity.this, getString(R.string.updated), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
+        fabDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+
+                        MainActivity.urls = (ArrayList<String>) value.get("urls");
+                        MainActivity.urlTitles = (ArrayList<String>) value.get("urlTitles");
+
+                        MainActivity.urlsArrayAdapter.notifyDataSetChanged();
+                        MainActivity.titlesArrayAdapter.notifyDataSetChanged();
+                        arrayAdapter.notifyDataSetChanged();
+
+                        Intent intent = new Intent(OnlineSavedRecipesActivity.this,OnlineSavedRecipesActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                    }
+                });
+
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.scorpysmurf.recipie2.mainactivity", Context.MODE_PRIVATE);
+                HashSet<String> urlSet= new HashSet<>(MainActivity.urls);
+                HashSet<String> titleSet = new HashSet<>(MainActivity.urlTitles);
+
+                sharedPreferences.edit().putStringSet("urls",urlSet).apply();
+                sharedPreferences.edit().putStringSet("titles",titleSet).apply();
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Toast.makeText(OnlineSavedRecipesActivity.this, getString(R.string.updated), Toast.LENGTH_SHORT).show();
+
     }
 }
